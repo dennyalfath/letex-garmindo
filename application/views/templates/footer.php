13b2990a8a0d556e34ad3d7cfc5c05ca9bdb14a9
Heri <herixr4@gmail.com>
2020-05-10 06:08:28 +0700

    Fix name in select status sales order detail

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -109,7 +109,7 @@
 
                 '<div class="col-md-3">' + '<div class="form-group">' + '<label for="desc">Description</label>' + '<input type="text" class="form-control" name="sod_desc[]">' + '</div>' + '</div>' +
 
-                '<div class="col-md-3">' + '<div class="form-group">' + '<label for="status">Status</label>' + '<select name="status[]" class="form-control" required>' + '<option value="0">-- Select status --</option>' + ' <option value="cut">On Cutting</option>' + '<option value="sew">On Sewing</option>' + '<option value="pack">On Packing</option>' + '<option value="sent">Sent Out</option>' + '<option value="cancelled">Cancelled</option>' + '</select>' + '</div>' + '</div>' +
+                '<div class="col-md-3">' + '<div class="form-group">' + '<label for="status">Status</label>' + '<select name="sod_status[]" class="form-control" required>' + '<option value="0">-- Select status --</option>' + ' <option value="cut">On Cutting</option>' + '<option value="sew">On Sewing</option>' + '<option value="pack">On Packing</option>' + '<option value="sent">Sent Out</option>' + '<option value="cancelled">Cancelled</option>' + '</select>' + '</div>' + '</div>' +
 
                 '<div class="col-md-3">' + '<div class="form-group">' + '<label for="sod_total_price">Total Price</label>' + '<input type="text" class="form-control" name="sod_total_price[]">' + '</div>' + '</div>' +
 
